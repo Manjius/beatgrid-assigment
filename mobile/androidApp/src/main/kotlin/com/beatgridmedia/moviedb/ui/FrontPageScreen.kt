95d5ee5e54c463215edb8d274e71f7be6de63604
Manjius <jon.irigoyen.c@gmail.com>
2026-02-08 17:25:40 +0100

    Fix Compose imports for padding and vertical scroll

--- mobile/androidApp/src/main/kotlin/com/beatgridmedia/moviedb/ui/FrontPageScreen.kt
+++ mobile/androidApp/src/main/kotlin/com/beatgridmedia/moviedb/ui/FrontPageScreen.kt
@@ -10,8 +10,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.verticalScroll
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
